a32d063a76dfdb030e6922ea58bed46fb915b46c
git <kontakt@bartoszzawadka.pl>
2018-01-29 00:39:41 +0100

    Added caliburn.micro

---
+++ NathalieInwentaryzacje/NathalieInwentaryzacje/Main/AppBootstrapper.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+using Caliburn.Micro;
+using NathalieInwentaryzacje.Main.Interfaces;
+using NathalieInwentaryzacje.ViewModels;
+
+namespace NathalieInwentaryzacje.Main
+{
+    public class AppBootstrapper : BootstrapperBase
+    {
+        private CompositionContainer _container;
+
+        public AppBootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void Configure()
+        {
+            _container = new CompositionContainer(
+                new AggregateCatalog(AssemblySource.Instance.Select(x => new AssemblyCatalog(x)))
+            );
+
+            var batch = new CompositionBatch();
+            var wm = new WindowManager();
+            batch.AddExportedValue<IWindowManager>(wm);
+            batch.AddExportedValue<IEventAggregator>(new EventAggregator());
+            batch.AddExportedValue(_container);
+            _container.Compose(batch);
+        }
+
+        /// <summary>
+        /// Gets the instance.
+        /// </summary>
+        /// <param name="serviceType">Type of the service.</param>
+        /// <param name="key">The key.</param>
+        /// <returns></returns>
+        protected override object GetInstance(Type serviceType, string key)
+        {
+            var contract = string.IsNullOrEmpty(key) ? AttributedModelServices.GetContractName(serviceType) : key;
+            var exports = _container.GetExportedValues<object>(contract);
+
+            if (exports.Any())
+                return exports.First();
+
+            throw new Exception($"Could not locate any instances of contract {contract}.");
+        }
+
+        /// <summary>
+        /// Override this to provide an IoC specific implementation.
+        /// </summary>
+        /// <param name="instance">The instance to perform injection on.</param>
+        protected override void BuildUp(object instance)
+        {
+            _container.SatisfyImportsOnce(instance);
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            Application.ShutdownMode = ShutdownMode.OnExplicitShutdown;
+
+            var batch = new CompositionBatch();
+            batch.AddExportedValue<IMain>(new MainViewModel());
+            _container.Compose(batch);
+            DisplayRootViewFor<IMain>();
+        }
+    }
+}
---
+++ NathalieInwentaryzacje/NathalieInwentaryzacje/ViewModels/MainViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+using NathalieInwentaryzacje.Main.Interfaces;
+
+namespace NathalieInwentaryzacje.ViewModels
+{
+    public class MainViewModel : PropertyChangedBase, IMain
+    {
+        
+    }
+}
