3345867121257931633af3832185cc022e24f7ae
git <kontakt@bartoszzawadka.pl>
2018-02-05 01:17:02 +0100

    Added reporting project

---
+++ NathalieInwentaryzacje/Lib/NathalieInwentaryzacje.Lib.Contracts/Dto/Reports/RecordEntryReportInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Data;
+
+namespace NathalieInwentaryzacje.Lib.Contracts.Dto.Reports
+{
+    public class RecordEntryReportInfo
+    {
+        public string RecordDate { get; }
+
+        public string RecordDisplayName { get; }
+
+        public DataTable RecordEntryTable { get; }
+
+        public RecordEntryReportInfo(string recordDate, string recordDisplayName, DataTable recordEntryTable)
+        {
+            RecordDate = recordDate;
+            RecordDisplayName = recordDisplayName;
+            RecordEntryTable = recordEntryTable;
+        }
+    }
+}
---
+++ NathalieInwentaryzacje/Lib/NathalieInwentaryzacje.Lib.Reporting/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("NathalieInwentaryzacje.Lib.Reporting")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NathalieInwentaryzacje.Lib.Reporting")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
+// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+[assembly: Guid("991dda27-8d71-4127-8389-bf16384afe7f")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Rewizja
+//
+// Możesz określić wszystkie wartości lub użyć domyślnych numerów kompilacji i poprawki
+// przy użyciu symbolu „*”, tak jak pokazano poniżej:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
